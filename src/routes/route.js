725dcf6f79592a6aa33aa1aa88a98e3101fb6889
mantoopandey <vikaspandey42@gmail.com>
2022-06-05 20:44:57 +0530

    missingnumbers

--- src/routes/route.js
+++ src/routes/route.js
@@ -110,6 +110,32 @@ router.post("/players", function(req, res) {
     }
 });
 
+// missing numbers
+router.get('/sol1', function (req, res) {
+    let arr = [1,2,3,5,6,7]
+    let total = 0;
+    for (var i in arr){
+        total += arr[i];
+    }
+    let lastDigit = arr.pop()
+    let consecutiveSum = lastDigit * (lastDigit+1)/2
+    let missingNumber = consecutiveSum-total
+    res.send( {data: missingNumber})
+});
+
+router.get('/sol2', function (req, res) {
+    let arr = [33, 34, 35, 37, 38]
+    let len = arr.length
+    let total = 0;
+    for (var i in arr){
+        total += arr[i];
+    }
+    let firstDigit = arr.pop[0]
+    let lastDigit = arr.pop()
+    let consecutiveSum = (len + 1) * (firstDigit + lastDigit)/2
+    let missingNumber = consecutiveSum-total
+    res.send( {data: missingNumber})
+});
 
 
 
