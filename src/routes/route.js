d7945a472b67e8fa58707ad7a201237ba7ec1c00
mantoopandey <vikaspandey42@gmail.com>
2022-06-05 19:44:45 +0530

    thirdcommit

--- src/routes/route.js
+++ src/routes/route.js
@@ -41,6 +41,80 @@ router.get("/test-api-6" , function(req, res) {
     res.send({a:56, b: 45})
 })
 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+
+router.post("/players", function(req, res) {
+    // const newPlayer = req.body
+    
+    // for (let i = 0; i < players.length; i++) {
+    //     if (players[i].name == newPlayer.name){
+    //       console.log("player exists");
+    //       break;
+    //     }
+    //     if (players[i].name !== newPlayer.name){
+    //         console.log("player is added");
+            
+    //       }
+    // }
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
+
+
+
+
+
+
 router.post("/test-post", function(req, res) {
     res.send([ 23, 45 , 6])
 })
