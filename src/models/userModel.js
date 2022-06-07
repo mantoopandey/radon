a7938694b31cfb06f9a2241e5ad26881c579d429
mantoopandey <vikaspandey42@gmail.com>
2022-06-07 10:19:25 +0530

    commit7

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,59 @@
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
+//     // isIndian: Boolean,
+//     // parentsInfo: {
+//     //     motherName: String,
+//     //     fatherName: String,
+//     //     siblingName: String
+//     // },
+//     // cars: [ String  ]
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema) //users
+
+
+
+// // String, Number
+// // Boolean, Object/json, array
+
+
+
+
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
+const bookSchema = new mongoose.Schema({
+    bookname : {
+        type : String,
+        unique : true,
+        required : true
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+    authorname : {
+        type : String,
+        unique : true,
+        required : true
     },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    category : {
+        type : String,
+        unique : true,
+        required : true
+    },
+    year :{
+        type : Number,
+        required : true
+    }
 }, { timestamps: true });
-
-module.exports = mongoose.model('User', userSchema) //users
-
-
-
-// String, Number
-// Boolean, Object/json, array
+module.exports = mongoose.model('Book', bookSchema)
