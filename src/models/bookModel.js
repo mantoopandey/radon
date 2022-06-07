6fc24128883225de0ba927efa919fc8389d5cd25
mantoopandey <vikaspandey42@gmail.com>
2022-06-07 10:02:32 +0530

    5commit

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,16 +1,53 @@
 const mongoose = require('mongoose');
 
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
+//     // isIndian: Boolean,
+//     // parentsInfo: {
+//     //     motherName: String,
+//     //     fatherName: String,
+//     //     siblingName: String
+//     // },
+//     // cars: [ String  ]
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema) //users
+
+
+
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
+    bookName: {
+        type: String,
+        required: true
+    }, 
     authorName: String, 
     tags: [String],
     
-    isPublished: Boolean,
+     isPublished:Boolean,
     prices: {
         indianPrice: String,
-        europePrice: String,
+        europePrice: String
     },
-    sales: {type: Number, default: 10}
+    totalPages:Number,
+    stockAvailable: Boolean,
+    year : {
+        type :Number,
+        default :2021
+    }
+
+    // sales: {type: Number, default: 10}
 }, { timestamps: true });
 
 
